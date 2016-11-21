708c90b7a8d5d5b09ddd4f7db38a485f671f8a00
Tiago Henriques <trinosauro@gmail.com>
2016-11-21 17:29:40 +0100

    Add minimal react example

---
+++ pages/minimal_react_example.js
@@ -0,0 +1,17 @@
+// adapted from
+// https://github.com/brianstone/gatsby-starter-clean/blob/master/pages/index.js
+
+import React from 'react'
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hello World!
+        </h1>
+        <p>Welcome to your new clean Gatsby site</p>
+      </div>
+    )
+  }
+} 
